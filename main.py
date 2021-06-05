ff4b44f719b056490075c7d417683b4eb52ef202
SyedFairozPasha <>
2021-06-05 20:03:35 +0000

    updated version

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print("Hello Python")
-x =200
-print(x)
+#integer divide
+divnumber = 13//3
+print(divnumber)
+#floating point division
+divnumber =13/3
+print(divnumber)
+
+var1 = 17.5
+var2=5
+varresult =var1+var2-9
+print(varresult)
+
+#strings
+str1="Sample testing"
+str2='Test message'
+str3="Hello! 'How are you'"
+str4='Different type "Great"'
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print("typing out string\t" +str(varresult))
+
